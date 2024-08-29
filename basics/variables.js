7a0a11052cd4aa3914204bbb7383fdd263322191
Abhi5318 <99983869+Abhi5318@users.noreply.github.com>
2024-08-29 11:59:17 +0000

    data types

--- basics/variables.js
+++ basics/variables.js
@@ -2,3 +2,13 @@ name ="abhi"
 let email ="abhi@5318.gmail.com"
 var password = 212121
 console.table([name,email,password])
+
+let value= 3
+let negValue= -value
+console.log(negValue)
+
+console.log("1"+2+2)
+
+const id= Symbol('123')
+const anotherId = Symbol('123')
+console.log(id===anotherId)
