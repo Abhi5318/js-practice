4a4e26cea3b62eac4fdfc8b68bd0f4e04cf3c231
Abhi5318 <99983869+Abhi5318@users.noreply.github.com>
2024-08-30 06:17:58 +0000

    string

--- basics/variables.js
+++ basics/variables.js
@@ -12,3 +12,19 @@ console.log("1"+2+2)
 const id= Symbol('123')
 const anotherId = Symbol('123')
 console.log(id===anotherId)
+
+
+const name1 = "Abhishek"
+const age = 24
+console.log(`Hello myself ${name1.toUpperCase()} & my age is ${age}`);
+
+const url = "http://singhabhi.com/abhishek%20singh"
+
+console.log(url.replace('%20','-')) 
+console.log(url.includes('abhi'))
+console.log(url.length)
+console.log(url.toUpperCase())
+console.log(url.charAt(8))
+console.log(url.split('-'))
+console.log(url.slice('4','12'))
+console.log(url.slice('-37','12'))
